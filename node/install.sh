3362ea791d5c1c16bb3e5eb787a6aaf7b76e51af
Daniel Forsberg <danielforsberg89@gmail.com>
2022-03-25 09:44:01 +0100

    fix: correct nvm version

--- node/install.sh
+++ node/install.sh
@@ -1,6 +1,6 @@
 # !/bin/bash
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
 export NVM_DIR=$HOME/.nvm;
 source $NVM_DIR/nvm.sh;
