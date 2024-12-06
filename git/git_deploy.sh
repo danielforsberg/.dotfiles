4d0b094f18fe6c17443f3176e45026c27af8c42d
Daniel Forsberg <danielforsberg89@gmail.com>
2024-12-06 10:24:01 +0100

    📜 Check it twice and sign the scroll

---
+++ git/git_deploy.sh
@@ -0,0 +1,23 @@
+seasonal_deploy=(
+"Deploy 🎅"
+"Deploy 🎁"
+"Deploy 🌟"
+"Deploy ❄️"
+"Deploy 🛷"
+"Deploy 🕯️"
+"Deploy 🍪"
+"Deploy 🌌"
+"Deploy 🦌"
+"Deploy 🎶"
+"Deploy 🍬"
+"Deploy 🎄"
+"Deploy 📜"
+"Deploy ⛄"
+"Deploy 💫"
+)
+
+# Select a random instruction
+random_index=$(( RANDOM % ${#seasonal_deploy[@]} ))
+title="${seasonal_deploy[$random_index]}"
+
+gh pr create --base master --title "$title"
